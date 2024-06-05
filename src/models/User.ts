a7cc3af006634f954d0e5492ee1477c622ae8164
mahmadmanzoor <62117177+mahmadmanzoor@users.noreply.github.com>
2024-06-06 04:27:47 +0500

    Initial commit

---
+++ src/models/User.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Content } from "next/font/google";
+
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date
+
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMesagge: boolean;
+    message: Message[]
+
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        default: Date.now
+    }
+})
